a45524a2f7dbc6bb7dcf7703673e7e85c707fe3c
LC <sophine_lc@163.com>
2019-04-04 13:33:44 +0800

    django

--- functional_tests2.py
+++ functional_tests2.py
@@ -14,7 +14,7 @@ class NewVisitorTest2(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later2(self):
         self.browser.get('http://127.0.0.1:8000')
 
-        self.assertIn('Django',self.browser.title)
+        self.assertIn('To-Do lists',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
 
--- lists/tests3.py
+++ lists/tests3.py
@@ -16,6 +16,7 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(html,expected_html)
 
+
     def test_home_page_returns_correct_html(self):
 
         response = self.client.get('/')
