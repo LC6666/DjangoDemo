c8f6205a6b9b1118e630993759ad1ec00f93ed98
LC <sophine_lc@163.com>
2019-04-04 14:44:51 +0800

    django

--- lists/views.py
+++ lists/views.py
@@ -9,5 +9,15 @@ from django.http import HttpResponse
 # def home_page(request):
 #     return HttpResponse('<html><title>Django</title></html>')
 
+# ======================test3--test4=======================
+# def home_page(request):
+#     if request.method=='POST':
+#         return HttpResponse(request.POST['item_text'])
+#     return render(request,'home.html')
+
+# ======================test5=======================
 def home_page(request):
-    return render(request,'home.html')
+    return render(request,
+                  'home.html',{
+                      'new_item_text':request.POST.get('item_text',''),
+                  })
