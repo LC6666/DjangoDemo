667a725a989af3ad04ade5c51bb54bfe7194fb54
LC <sophine_lc@163.com>
2019-04-04 13:13:02 +0800

    django

---
+++ lists/tests3.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.views import home_page
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html,expected_html)
+
+    def test_home_page_returns_correct_html(self):
+
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>To-Do lists</title>',html)
+        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response,'home.html')
