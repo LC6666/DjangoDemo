1e5c21b645812c01bb310b416b333bcb20a957ca
LC <sophine_lc@163.com>
2019-04-04 12:26:45 +0800

    django

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
-def home_page():
-    pass
+# =======================test1===========================
+# def home_page():
+#     pass
+
+def home_page(request):
+    return HttpResponse('<html><title>Django</title></html>')
