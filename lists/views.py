938f43a027c195289d406dabba06048bd0cb1c94
LC <sophine_lc@163.com>
2019-04-08 10:44:32 +0800

    django

--- lists/views.py
+++ lists/views.py
@@ -64,8 +64,9 @@ def home_page(request):
     if request.method=='POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
-    items = Item.objects.all()
-    return render(request,'home.html',{"items":items})
+    # items = Item.objects.all()
+    # return render(request,'home.html',{"items":items})
+    return render(request,'home.html')
 
 # ======================test12=======================
 def view_list(request):
